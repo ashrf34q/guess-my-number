cea80964ac18c3e72f66834a380d04464a4ca77e
Ashraf Makki <makkiashraf08@gmail.com>
2023-03-30 22:59:09 -0400

    Refactoring

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+
+const displayMessage = (message) => {
+  document.querySelector('.message').textContent = message;
+}
+
 // Check event listener
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   //   No number is entered
   if (!guess) {
-    document.querySelector('.message').textContent = '😡 no number entered';
+     displayMessage('😡 no number entered');
   }
   //   correct guess
   else if (guess === randomNumber) {
-    document.querySelector('.message').textContent = 'Correct number';
+    displayMessage('Correct number');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '45rem';
 
@@ -27,11 +32,11 @@ document.querySelector('.check').addEventListener('click', function () {
   else{
     if(score > 0) { 
       score--;
-      document.querySelector('.message').textContent = guess > randomNumber ? '📈 Too High' : '📉 Too Low';
+      displayMessage(guess > randomNumber ? '📈 Too High' : '📉 Too Low');
       document.querySelector('.score').textContent = score;
     }
     else { // score === 0
-      document.querySelector('.message').textContent = 'You lost the game';
+      displayMessage('You lost the game');
     }
   }
 });
@@ -49,7 +54,7 @@ document.querySelector(".again").addEventListener('click', function () {
   randomNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
 
-  document.querySelector('.message').textContent = 'Start guessing ...';
+  displayMessage('Start guessing ...');
   document.querySelector('body').style.backgroundColor = "#222";
   document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
